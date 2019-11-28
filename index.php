9fb9341062926631b85043956d7cf405f8dac260
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-11-28 22:19:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,98 +2,10 @@
 
 require "config.php";
 require "Userdetails.php";
-
-//Facebook Login Functions
-//$redirectURL = "http://".$_SERVER['SERVER_NAME']."/fb_callback.php";
-//$redirectURL = "http://localhost/UI_project/fb_callback.php";
-//$permissions = ['email'];
-//$login_Url = $helper->getLoginUrl($redirectURL, $permissions);
-
-require 'vendor/autoload.php';
-
-$helper = $FB->getRedirectLoginHelper();
-
-$permissions = ['email']; // optional
-try {
-
-if (isset($_SESSION['facebook_access_token'])) {
-$accessToken = $_SESSION['facebook_access_token'];
-} else {
-  $accessToken = $helper->getAccessToken();
-}
-
-} catch(Facebook\Exceptions\facebookResponseException $e) {
-// When Graph returns an error
-echo 'Graph returned an error: ' . $e->getMessage();
-  exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-// When validation fails or other local issues
-echo 'Facebook SDK returned an error: ' . $e->getMessage();
-  exit;
-}
-
-if (isset($accessToken)) {
-if (isset($_SESSION['facebook_access_token'])) {
-$FB->setDefaultAccessToken($_SESSION['facebook_access_token']);
-} else {
-// getting short-lived access token
-$_SESSION['facebook_access_token'] = (string) $accessToken;
-  // OAuth 2.0 client handler
-$oAuth2Client = $FB->getOAuth2Client();
-// Exchanges a short-lived access token for a long-lived one
-$longLivedAccessToken = $oAuth2Client->getLongLivedAccessToken($_SESSION['facebook_access_token']);
-$_SESSION['facebook_access_token'] = (string) $longLivedAccessToken;
-// setting default access token to be used in script
-$FB->setDefaultAccessToken($_SESSION['facebook_access_token']);
-}
-
-// redirect the user to the profile page if it has "code" GET variable
-if (isset($_GET['code'])) {
-header('Location: hello.php');
-exit();
-}
-// getting basic info about user
-
-/*
-try {
-$profile_request = $FB->get('/me?fields=name,first_name,last_name,email');
-$requestPicture = $FB->get('/me/picture?redirect=false&height=200'); //getting user picture
-$picture = $requestPicture->getGraphUser();
-$profile = $profile_request->getGraphUser();
-$fbid = $profile->getProperty('id');           // To Get Facebook ID
-$fbfullname = $profile->getProperty('name');   // To Get Facebook full name
-$fbemail = $profile->getProperty('email');    //  To Get Facebook email
-$fbpic = "<img src='".$picture['url']."' class='img-rounded'/>";
-# save the user nformation in session variable
-$_SESSION['fb_id'] = $fbid.'</br>';
-$_SESSION['fb_name'] = $fbfullname.'</br>';
-$_SESSION['fb_email'] = $fbemail.'</br>';
-$_SESSION['fb_pic'] = $fbpic.'</br>';
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-// When Graph returns an error
-echo 'Graph returned an error: ' . $e->getMessage();
-session_destroy();
-// redirecting user back to app login page
-header("Location: test_fb.php");
-exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-// When validation fails or other local issues
-echo 'Facebook SDK returned an error: ' . $e->getMessage();
-exit;
-}
-} else {
-// replace your website URL same as added in the developers.Facebook.com/apps e.g. if you used http instead of https and you used   
-*/ 
-}  
-      
-$fb_login_Url = $helper->getLoginUrl('http://localhost/UI_project/test_fb.php', $permissions);
-
-
+require './../../vendor/autoload.php';
 
 //Google Login Url
 $loginURL = $gClient->createAuthUrl();
-
-
 
 
 //User login Data
