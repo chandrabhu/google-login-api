94a07e3e486a6611ab1d5af1e93a1144b6b2b7ef
Chandra Bhushan Sharma <chandrabhu24@gmail.com>
2019-11-28 22:15:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,32 +1,19 @@
 <?php
 
 session_start();
-require "vendor/autoload.php";
+require "./../../vendor/autoload.php";
 
 
 /*  Google API login Setup   */
 $gClient = new Google_CLient();
-$gClient->setClientId("682481523886-e12tqjnqk6u6o08q2ir0s45lsn5mvmco.apps.googleusercontent.com");
+$gClient->setClientId("6824815212345-e12tqjnqk1234508q2ir0s45lsn5mvmco.apps.googleusercontent.com");
 $gClient->setClientSecret("SZkijvf0F_8ldQ7wR5N0RdUq");
 $gClient->setApplicationName("CPI Login Tutorial");
-$gClient->setRedirectUri("http://localhost/UI_project/g_callback.php");
+$gClient->setRedirectUri("http://localhost/PROJECT_NAME/g_callback.php");
 
 //Google Authorization to get Details 
 $gClient->addScope("https://www.googleapis.com/auth/plus.login");
 $gClient->addScope("https://www.googleapis.com/auth/userinfo.email");
 
-
-/* Facebook API login Setup  */
-$FB = new \Facebook\Facebook([
- 'app_id' => '378095093050017',
- 'app_secret'=> '229db002854cca6d04d0ea8e9aff9bfb',
- 'default_graph_version' => 'v3.3'
-]);
-
-
-//$helper = $FB->getRedirectLoginHelper();
-//if(isset($_GET['state'])){
-//$helper->getPersistentDataHandler()->set('state',$_GET['state']);
-//}
 
 ?>
